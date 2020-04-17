c6bdfbb7e5454961022284af498a1a6835e5660c
lbetol <llbetoll@hotmail.com>
2020-04-17 15:07:47 -0300

    crawler

--- iniciar.py
+++ iniciar.py
@@ -1,11 +1,9 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-#from PyQt4.QtCore import QTimer
-
 from semantix.spiders.ibovespa import IbovespaSpider
 from semantix.spiders.nasdaq import NasdaqSpider
 from semantix.spiders.usdbrl import UsdBrlSpider
-
+import threading
 
 process = CrawlerProcess(get_project_settings())
 process.crawl(NasdaqSpider)
@@ -13,21 +11,12 @@ process.crawl(IbovespaSpider)
 process.crawl(UsdBrlSpider)
 process.start()
 
+def iniciar():
+    process.start()
 
-#def crawler():
-#    iniciar = CrawlerProcess(get_project_settings())
-#    iniciar.crawl(IbovespaSpider)
-#    iniciar.start()
+threading.Timer(120, iniciar).start()
 
-# iniciar = CrawlerProcess(get_project_settings())
- #   iniciar.crawl(NasdaqSpider)
-  #  iniciar.start()
 
-   # iniciar = CrawlerProcess(get_project_settings())
-    # iniciar.crawl(UsdBrlSpider)
-   # iniciar.start()
 
-    #timer = QTimer()
-    #timer.setSingleShot(True)
-    #timer.timeout.connect(crawler)
-    #timer.start(120000)
+
+
