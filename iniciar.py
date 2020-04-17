71a35b2980c037c2d8598612bf526d8fa7dca27a
lbetol <llbetoll@hotmail.com>
2020-04-17 16:57:49 -0300

    crawler

--- iniciar.py
+++ iniciar.py
@@ -1,3 +1,10 @@
+# Projeto web crawler
+# Processo seletivo para a empresa Semantix
+# ------------------------------------------
+# Autor: Alberto Barrios
+# Data: 11/04/2020
+# ------------------------------------------
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from semantix.spiders.ibovespa import IbovespaSpider
@@ -5,18 +12,16 @@ from semantix.spiders.nasdaq import NasdaqSpider
 from semantix.spiders.usdbrl import UsdBrlSpider
 import threading
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(NasdaqSpider)
-process.crawl(IbovespaSpider)
-process.crawl(UsdBrlSpider)
-process.start()
 
-def iniciar():
+def temporizador():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(NasdaqSpider)
+    process.crawl(IbovespaSpider)
+    process.crawl(UsdBrlSpider)
     process.start()
 
-threading.Timer(120, iniciar).start()
+    t = threading.Timer(25, temporizador)
+    t.start()
+    temporizador()
 
-
-
-
-
+temporizador()
